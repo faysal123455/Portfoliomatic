a368c00c46eaf0dd6a07911825839d86f75346d5
Faysal Sarker <faysal.sarker@pernod-ricard.com>
2023-11-06 00:25:22 +0100

    add formulair

--- frontend/src/views/public/AboutPage.jsx
+++ frontend/src/views/public/AboutPage.jsx
@@ -1,13 +1,23 @@
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
+import "./AboutPage.css";
+
 
 const AboutPage = ()  => {
-    return <div className="container">
-        <Header/>
-    <h1>this is about page</h1>
-    f
-        <Footer/>
+    return <div className="container_about">
+        
+        <section className="about">
+            <form method="post">
+                <input type="text" placeholder="Name" id ="name" autoComplete="off"></input>
+                <input type="email" placeholder="Email" id ="email" autoComplete="off"></input>
+                <input type="text" placeholder="Subject" id="subject" autoComplete="off"></input>
+                <textarea id="message" placeholder="Message" cols="30" rows="30"></textarea>
+                <button id="btn">Send</button>
+            </form>
 
+        </section>
+        <Header/>
+        <Footer/>
     </div>
 }
 
